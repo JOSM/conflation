061b390e2a84f1301ecbfe0c7e4a65a79719fd03
Vincent Privat <vincent.privat@gmail.com>
2019-04-14 12:11:03 +0200

    see #josm17588 - remove deprecated api (patch by taylor.smock)

--- src/org/openstreetmap/josm/plugins/conflation/matcher/LevenshteinDistanceValueMatcher.java
+++ src/org/openstreetmap/josm/plugins/conflation/matcher/LevenshteinDistanceValueMatcher.java
@@ -1,7 +1,7 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.plugins.conflation.matcher;
 
-import org.openstreetmap.josm.data.validation.tests.SimilarNamedWays;
+import org.openstreetmap.josm.tools.Utils;
 
 public class LevenshteinDistanceValueMatcher implements ValueMatcher {
 
@@ -23,6 +23,6 @@ public class LevenshteinDistanceValueMatcher implements ValueMatcher {
         if (maxLength == 0)
             return 1.0;
         double divider = (distanceThreshold > 0) ? distanceThreshold : maxLength;
-        return Math.max(0.0, 1.0 - ((double) SimilarNamedWays.getLevenshteinDistance(target, candidate) / divider));
+        return Math.max(0.0, 1.0 - ((double) Utils.getLevenshteinDistance(target, candidate) / divider));
     }
 }
